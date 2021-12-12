b5b805d3fd6a9a439edc4a6bc925c0f23c633e92
KelemenBoglarka <90687000+KelemenBoglarka@users.noreply.github.com>
2021-12-12 13:16:01 +0200

    HF4

---
+++ HF4/Library & Books/Book.php
@@ -0,0 +1,43 @@
+<?php
+namespace book;
+use abstractlibrary\AbstractLibrary;
+use author\Author;
+use library\Library;
+
+
+class Book
+{
+    public string $title;
+    public float $price;
+    public Author $author;
+
+    public function __construct(string $title, float $price, string $author) {
+        $this->title = $title;
+        $this->price = $price;
+        $this->author = $author;
+    }
+    function getTitle(): string {
+        return $this->title;
+    }
+
+    function getPrice(): float {
+        return $this->price;
+    }
+
+    function getAuthor(): String {
+        return $this->author;
+    }
+
+    function setTitle(string $title): void {
+        $this->title = $title;
+    }
+
+    function setPrice(float $price): void {
+        $this->price = $price;
+    }
+
+    function setAuthor(String $author): void {
+        $this->author = $author;
+    }
+
+}
---
+++ HF4/Library & Books/Library.php
@@ -0,0 +1,56 @@
+<?php
+namespace library;
+use abstractlibrary\AbstractLibrary;
+use book\Book;
+use author\Author;
+require_once "AbstractLibrary.php";
+class Library extends AbstractLibrary
+{
+    public function getAuthors(): array {
+        return parent::getAuthors();
+    }
+
+    public function setAuthors(string $authors): void {
+        parent::setAuthors($authors);
+    }
+
+    public function addAuthor(string $authorName): Author {
+        $a1 = new Author($authorName);
+        $this->setAuthors($authorName);
+        return $a1;
+    }
+    public function addBookForAuthor($authorName, Book $book) {
+
+        foreach ($this->getAuthors() as $auth) {
+            if ($authorName == $auth) {
+                $auth->setBooks($book->getTitle(), $book->getPrice());
+            }
+        }
+    }
+
+    public function getBooksForAuthor($authorName) {
+        foreach ($this->getAuthors() as $value) {
+            if($value==$authorName){
+                return $value->getBooks();
+            }
+        }
+        return "Nem létező író";
+    }
+    public function search(string $bookName): Book {
+        foreach ($this->getAuthors()->getBooks() as $value){
+            if($value==$bookName){
+                return $value;
+            }
+        }
+        return "Nincs létező" ;
+    }
+    public function print()
+    {
+        foreach ($this->getAuthors() as $value) {
+            print "$value <br>-<br>";
+            foreach ($value->getBooks() as $book) {
+                print $book->getTitle() . " - " . $book->getPrice() . "<br>";
+            }
+        }
+    }
+}
